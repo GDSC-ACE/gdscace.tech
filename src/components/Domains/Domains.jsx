55ffed9c70303ebf128452eadb419ebf5fc155e4
“Akash <kolakkalakash@gmail.com>
2024-02-19 18:15:32 +0530

    partial import fix

--- src/components/Domains/Domains.jsx
+++ src/components/Domains/Domains.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "../../styles/global.css";
-import Img from "../../assets/react.svg";
-import HoverImg from "../../assets/ufo.png"
 
 const Domains = () => {
   return (
@@ -12,24 +10,24 @@ const Domains = () => {
         </div>
         <div>
           <div className="flex flex-row ml-[170px]" >
-            <img src={HoverImg} className="w-48 h-44" alt="This if for Hover" />
+            <img src={"./assets/ufo.png"} className="w-48 h-44" alt="This if for Hover" />
           </div>
         </div>
         <div className="flex flex-row justify-evenly ">
 
           <div className="border-stone-200 border-2 h-64 w-64 rounded-lg flex flex-col">
             <h2 className="text-white text-4xl text-center mb-10 mt-5">AI / ML</h2>
-            <img src={Img} className="h-28 flex justify-center " alt="AIML" />
+            <img src={"../../assets/ufo.png"} className="h-28 flex justify-center " alt="AIML" />
           </div>
 
           <div className="border-stone-200 border-2 h-64 w-64 rounded-lg flex flex-col">
             <h2 className="text-white text-4xl text-center mb-10 mt-5">Blockchain</h2>
-            <img src={Img} className="h-28 flex justify-center " alt="AIML" />
+            <img src={"/assets/ufo.png"} className="h-28 flex justify-center " alt="AIML" />
           </div>
 
           <div className="border-stone-200 border-2 h-64 w-64 rounded-lg flex flex-col">
             <h2 className="text-white text-4xl text-center mb-10 mt-5">Web/App</h2>
-            <img src={Img} className="h-28 flex justify-center " alt="AIML" />
+            <img src={"/assets/ufo.png"} className="h-28 flex justify-center " alt="AIML" />
           </div>
 
         </div>
@@ -38,12 +36,12 @@ const Domains = () => {
 
           <div className="border-stone-200 border-2 h-64 w-64 rounded-lg flex flex-col">
             <h2 className="text-white text-4xl text-center mb-10 mt-5">Social Causes</h2>
-            <img src={Img} className="h-28 flex justify-center " alt="AIML" />
+            <img src={"/assets/ufo.png"} className="h-28 flex justify-center " alt="AIML" />
           </div>
 
           <div className="border-stone-200 border-2 h-64 w-64 rounded-lg flex flex-col">
             <h2 className="text-white text-4xl text-center mb-10 mt-5">CyberSecurity</h2>
-            <img src={Img} className="h-28 flex justify-center " alt="AIML" />
+            <img src={"/assets/ufo.png"} className="h-28 flex justify-center " alt="AIML" />
           </div>
         </div>
       </div>
