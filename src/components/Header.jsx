a0ca64215b006ed4714e10ba35140c14a6f31557
Sahil wagh <Sahilwagh142@gmail.com>
2024-02-21 20:43:57 +0530

    style-boder_line_tree_img_date_changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="flex items-start p-[30px]  justify-center  md:justify-between">
+    <header className="flex items-start m-[10px] mb-0 p-[20px] border-t-2 border-r-2 border-yellow-500 justify-center  md:justify-between">
       <figure className="flex items-start gap-[20px]">
         <div className="flex items-start">
           <img src="/assets/logo.svg" alt="logo" />
