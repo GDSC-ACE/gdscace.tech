66a5e42c57b6ed70ab366c45564aac1bc91b88d7
adityasansare <119444881+adityasansare@users.noreply.github.com>
2024-02-24 16:08:47 +0530

    hower effect on sponsors

--- src/components/home/partials/SponsorCard.jsx
+++ src/components/home/partials/SponsorCard.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import "./../../../index.css"; 
+
 
 const SponsorCard = ({ sponsorName, imagePath, ringImagePath }) => {
   return (
     <div className="domain-card m-2 flex flex-col items-center relative">
+     
       <img
         src={ringImagePath}
         className="absolute inset-0 z-0 object-cover"
         style={{ width: '100%', height: '80%', borderRadius: '50%' }}
         alt="Circular ring"
       />
-      <div className="h-32 w-32 lg:h-60 lg:w-60 relative z-10 flex items-center justify-center overflow-hidden ">
+      
+      <div className="h-32 w-32 lg:h-60 lg:w-60 relative z-10 flex items-center justify-center overflow-hidden">
         <img
           src={imagePath}
-          className="h-20 sm:h-28 lg:h-40 z-10 object-cover"
+          className="h-20 sm:h-28 lg:h-40 z-10 object-cover custom-hover-effect"
           alt={sponsorName} 
         />
       </div>
