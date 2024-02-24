4e8f5398ded133d90def4d0016221deb34145589
Sachin Prajapati <captsachin8898@gmail.com>
2024-02-25 00:03:59 +0530

    Added Padding in the about.jsx Components

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,10 +1,13 @@
 const About = () => {
   return (
-    <section className="flex min-h-[100svh] flex-col items-center justify-center">
+    <section
+      id="about"
+      className="mx-auto flex min-h-[100svh] max-w-3xl flex-col items-center justify-center lg:max-w-5xl"
+    >
       <div className="mb-11 flex flex-row items-center justify-center">
         <h1 className="mb-9 text-3xl font-bold text-red-50">About us</h1>
       </div>
-      <div className="">
+      <div className="text-justify p-9">
         <p className="mb-5 text-center text-3xl text-white">
           Presenting ACE IEEE Techithon's Hack Mania, one of the biggest 24-Hour
           Offline Hackathon of ACE.
@@ -13,11 +16,11 @@ const About = () => {
           With this event, we plan to provide a platform for students from all
           over Mumbai where they can learn invaluable skills about product
           development by building projects from scratch and competing with
-          fellow students
+          fellow students.
         </p>
         <p className="mb-5 text-center text-3xl text-white">
           Our aim is to develop applications, website and solutions that would
-          be of utility for the society
+          be of utility for the society.
         </p>
       </div>
     </section>
